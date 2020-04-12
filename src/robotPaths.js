3ccc237fcff90eb520db0dfd8090101415fa4ff3
phuonggg <tieuphuong.ftu@gmail.com>
2020-04-12 12:06:48 +0900

    finished basic requirements

--- src/robotPaths.js
+++ src/robotPaths.js
@@ -23,12 +23,38 @@ class RobotPaths {
   // you may want to change this code later on, too
   constructor(size) {
     this.board = new Board(size);
+    this.size = size;
     this.row = 0;
     this.col = 0;
+    this.count = 0;
   }
 
   solve() {
     // Your code here.
+
+    const pathsCounter = (x, y) => {
+      if (x === this.size - 1 && y === this.size - 1) {
+        this.count++;
+        return;
+      }
+
+      if (x < 0 || y < 0 || x >= this.size || y >= this.size) {
+        return;
+      }
+
+      if (this.board.hasBeenVisited(x, y)) {
+        return;
+      }
+
+      this.board.togglePiece(x, y);
+      pathsCounter(x, y + 1); //right
+      pathsCounter(x + 1, y); //down
+      pathsCounter(x, y - 1); //left
+      pathsCounter(x - 1, y); //up
+      this.board.togglePiece(x, y);
+    };
+    pathsCounter(0, 0);
+    return this.count;
   }
 }
 
